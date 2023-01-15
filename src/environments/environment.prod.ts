a3836653d9441fdb59ad861d0e6ac48f5475b489
partheev <partheev8@gmail.com>
2023-01-15 16:52:33 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  backendURL:
+    'http://ec2-13-113-231-243.ap-northeast-1.compute.amazonaws.com/api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // backendURL: 'http://localhost:3001/api',
+  backendURL:
+    'http://ec2-13-113-231-243.ap-northeast-1.compute.amazonaws.com/api',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
